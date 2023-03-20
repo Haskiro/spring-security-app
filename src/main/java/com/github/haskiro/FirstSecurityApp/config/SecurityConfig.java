6751a951c6156c24f0bc1dae5a66734155a3c0c0
Павел <58857483+Haskiro@users.noreply.github.com>
2023-03-20 13:21:01 +0300

    authentication without custom authProvider

--- src/main/java/com/github/haskiro/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/com/github/haskiro/FirstSecurityApp/config/SecurityConfig.java
@@ -1,25 +1,34 @@
 package com.github.haskiro.FirstSecurityApp.config;
 
-import com.github.haskiro.FirstSecurityApp.security.AuthProviderImpl;
+import com.github.haskiro.FirstSecurityApp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return personDetailsService;
     }
 
     @Bean
@@ -32,7 +41,7 @@ public class SecurityConfig {
                 .permitAll()
                 .and()
                 .logout().permitAll()
-                .and().authenticationProvider(authProvider);
+                .and().userDetailsService(userDetailsService());
 
         return http.build();
     }
